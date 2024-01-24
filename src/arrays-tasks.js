fb10ad04cf4608126bddd5a8908479cb093fad2e
Dmitry Matul <matuldmtr@gmail.com>
2024-01-24 21:13:03 +0100

    feat: getAverage, isSameLength complete

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -124,8 +124,13 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (arr.length === 0) return 0;
+
+  const sum = arr.reduce((accu, item) => accu + item, 0);
+  const res = sum / arr.length;
+
+  return Number(res.toFixed(2));
 }
 
 /**
@@ -138,8 +143,8 @@ function getAverage(/* arr */) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  return arr.every((item) => item.length === arr[0].length);
 }
 
 /**
