402953b21556fc88685927b27b4dd895cd4f75b0
Dmitry Matul <matuldmtr@gmail.com>
2024-01-27 19:04:26 +0100

    feat: getIndicesOfOddNumbers, getHexRGBValues compleete

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -422,8 +422,11 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  return numbers.reduce((accu, item, index) => {
+    if (item % 2 !== 0) accu.push(index);
+    return accu;
+  }, []);
 }
 
 /**
@@ -436,8 +439,10 @@ function getIndicesOfOddNumbers(/* numbers */) {
  *    getHexRGBValues([ 0, 255, 16777215]) => [ '#000000', '#0000FF', '#FFFFFF' ]
  *    getHexRGBValues([]) => []
  */
-function getHexRGBValues(/* arr */) {
-  throw new Error('Not implemented');
+function getHexRGBValues(arr) {
+  return arr.map(
+    (item) => `#${item.toString(16).toUpperCase().padStart(6, '0')}`
+  );
 }
 
 /**
